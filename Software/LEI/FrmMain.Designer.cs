439c98de3d2a36b7fd26d0fccce75132ffcf92b5
jferko21 <jferko21@student.foi.hr>
2023-05-24 11:32:07 +0200

    Dodane labele u glavni form koje se ažuriraju svakih 2s.

--- Software/LEI/FrmMain.Designer.cs
+++ Software/LEI/FrmMain.Designer.cs
@@ -43,6 +43,12 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblElectrConsumption = new System.Windows.Forms.Label();
+            this.lblWaterConsumption = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblGasConsumption = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dvgObjects)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -92,14 +98,14 @@
             // btnAddRemoveObj
             // 
             this.btnAddRemoveObj.Name = "btnAddRemoveObj";
-            this.btnAddRemoveObj.Size = new System.Drawing.Size(180, 22);
+            this.btnAddRemoveObj.Size = new System.Drawing.Size(144, 22);
             this.btnAddRemoveObj.Text = "Dodaj/Ukloni";
             this.btnAddRemoveObj.Click += new System.EventHandler(this.btnAddRemoveObj_Click);
             // 
             // senzoriToolStripMenuItem
             // 
             this.senzoriToolStripMenuItem.Name = "senzoriToolStripMenuItem";
-            this.senzoriToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.senzoriToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
             this.senzoriToolStripMenuItem.Text = "Senzori";
             // 
             // korisniciToolStripMenuItem
@@ -167,12 +173,84 @@
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(395, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(333, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Generalna sveukupna potrošnja struje (po min):";
+            // 
+            // lblElectrConsumption
+            // 
+            this.lblElectrConsumption.AutoSize = true;
+            this.lblElectrConsumption.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblElectrConsumption.ForeColor = System.Drawing.Color.White;
+            this.lblElectrConsumption.Location = new System.Drawing.Point(496, 92);
+            this.lblElectrConsumption.Name = "lblElectrConsumption";
+            this.lblElectrConsumption.Size = new System.Drawing.Size(140, 39);
+            this.lblElectrConsumption.TabIndex = 9;
+            this.lblElectrConsumption.Text = "13 kWh";
+            // 
+            // lblWaterConsumption
+            // 
+            this.lblWaterConsumption.AutoSize = true;
+            this.lblWaterConsumption.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblWaterConsumption.ForeColor = System.Drawing.Color.White;
+            this.lblWaterConsumption.Location = new System.Drawing.Point(496, 245);
+            this.lblWaterConsumption.Name = "lblWaterConsumption";
+            this.lblWaterConsumption.Size = new System.Drawing.Size(140, 39);
+            this.lblWaterConsumption.TabIndex = 11;
+            this.lblWaterConsumption.Text = "13 kWh";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(395, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(330, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Generalna sveukupna potrošnja vode (po min):";
+            // 
+            // lblGasConsumption
+            // 
+            this.lblGasConsumption.AutoSize = true;
+            this.lblGasConsumption.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblGasConsumption.ForeColor = System.Drawing.Color.White;
+            this.lblGasConsumption.Location = new System.Drawing.Point(496, 375);
+            this.lblGasConsumption.Name = "lblGasConsumption";
+            this.lblGasConsumption.Size = new System.Drawing.Size(140, 39);
+            this.lblGasConsumption.TabIndex = 13;
+            this.lblGasConsumption.Text = "13 kWh";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(399, 335);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(329, 16);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Generalna sveukupna potrošnja plina (po min):";
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
             this.ClientSize = new System.Drawing.Size(853, 778);
+            this.Controls.Add(this.lblGasConsumption);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblWaterConsumption);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblElectrConsumption);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
@@ -207,5 +285,11 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblElectrConsumption;
+        private System.Windows.Forms.Label lblWaterConsumption;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblGasConsumption;
+        private System.Windows.Forms.Label label9;
     }
 }
--- Software/LEI/FrmMain.cs
+++ Software/LEI/FrmMain.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using LEICore.Consumption;
 using LEICore.Objects;
 using LEICore.Users;
 
@@ -18,6 +19,9 @@ namespace LEI
     {
         User user = new User();
         Chart chartWater = new Chart();
+        List<LEICore.Objects.Object> objlist = new List<LEICore.Objects.Object>();
+        static Timer reloadTimer = new Timer();
+
 
         public FrmMain(User user)
         {
@@ -28,10 +32,19 @@ namespace LEI
         private void FrmMain_Load(object sender, EventArgs e)
         {
             LoadData();
+            reloadTimer.Tick += new EventHandler(RealodTimerEventObject);
+
+            // Sets the timer interval to 5 seconds.
+            reloadTimer.Interval = 5000;
+            reloadTimer.Start();
         }
 
         private void LoadData() {
-            List<LEICore.Objects.Object> objlist = new List<LEICore.Objects.Object>();
+            LoadDvgObjects();
+            LoadConsumptionLabels();
+        }
+
+        private void LoadDvgObjects() {
             ObjectRepository objectRepository = new ObjectRepository();
 
             objlist = objectRepository.GetObjects(user.Id);
@@ -46,7 +59,7 @@ namespace LEI
                 //dvgObjects.Columns["SensorID"].Visible = false;
                 //dvgObjects.Columns["UserID"].Visible = false;
 
-                Chart myChart = new Chart();
+                /*Chart myChart = new Chart();
                 myChart.Series.Add("Data");
                 myChart.Series["Data"].Points.AddXY(0, 2);
                 myChart.Series["Data"].Points.AddXY(1, 5);
@@ -54,8 +67,45 @@ namespace LEI
                 myChart.Series["Data"].ChartType = SeriesChartType.Line;
 
 
-                this.Controls.Add(myChart);
+                this.Controls.Add(myChart);*/
             }
+        }
+
+        private void LoadConsumptionLabels() {
+            float waterconsumption, gasconsumption, electryconsumption;
+            ConsumptionRepository consumptionRepository = new ConsumptionRepository();
+            List<ConsumptionData> consumptionData = new List<ConsumptionData>();
+            DateTime timenow = DateTime.Now;
+
+            waterconsumption = gasconsumption = electryconsumption = 0;
+
+            foreach (LEICore.Objects.Object obj in objlist)
+            {
+                consumptionData = consumptionRepository.GetConsumptionsByObject(obj.Id);
+                if (consumptionData != null) {
+                    foreach (ConsumptionData cdata in consumptionData) {
+                        if (cdata.Date.Year == timenow.Year && cdata.Date.Month == timenow.Month &&
+                            cdata.Date.Day == timenow.Day && cdata.Date.Minute == timenow.Minute)
+                        {
+                            if (cdata.ConsumptionType == ConsumptionData.consumptionType.Water)
+                                waterconsumption += cdata.ConsumptionValue;
+                            else if (cdata.ConsumptionType == ConsumptionData.consumptionType.Gas)
+                                gasconsumption += cdata.ConsumptionValue;
+                            else if (cdata.ConsumptionType == ConsumptionData.consumptionType.Electricity)
+                                electryconsumption += cdata.ConsumptionValue;
+                        }
+                    }
+                }
+            }
+
+            lblWaterConsumption.Text = waterconsumption.ToString() + " kWh";
+            lblGasConsumption.Text = gasconsumption.ToString() + " L/s";
+            lblElectrConsumption.Text = electryconsumption.ToString() + " L/s";
+        }
+
+        private void RealodTimerEventObject 
+            (System.Object myObject, EventArgs myEventArgs) {
+            LoadData();
         }
 
         private void btnLogOut_Click(object sender, EventArgs e)
